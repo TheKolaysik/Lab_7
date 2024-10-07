f1f3fe8b003768e496628b8e50693caddc4c010b
Kolaysik <tolstyhnikolai@gmail.com>
2024-10-07 13:09:05 +0300

    задание 1 + 2

--- Lab_7.c
+++ Lab_7.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
 
 void main() {
 	setlocale(LC_ALL, "RUS");
-	char c;
+	char c; /*
 	scanf("%c", &c);
 	switch (c) {
 		case '0':
@@ -38,6 +39,25 @@ void main() {
 			break;
 		default:
 			printf("это буква");
+			break;
+	} */
+	float x, y;
+	scanf("%f%c%f", &x, &c, &y);
+	switch (c) {
+		case '+':
+			printf("%f", x + y);
+			break;
+		case '-':
+			printf("%f", x - y);
+			break;
+		case '/':
+			printf("%f", x / y);
+			break;
+		case '*':
+			printf("%f", x * y);
+			break;
+		case '^':
+			printf("%f", pow(x, y));
 			break;
 	}
 }
